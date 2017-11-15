267d6d8570ba42f43ee810b8a448305ba7b62c4d
Dmitry Jemerov <yole@jetbrains.com>
2017-11-15 12:26:32 +0100

    Fetch and render new messages

--- backend/src/main/kotlin/Server.kt
+++ backend/src/main/kotlin/Server.kt
@@ -7,20 +7,31 @@ import io.ktor.content.resources
 import io.ktor.content.static
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
+import kotlinx.serialization.json.JSON
 import java.util.*
 
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.response.status(HttpStatusCode.InternalServerError)
+        }
+    }
     install(Routing) {
         get("/") {
             call.respondText(renderIndexPage(), ContentType.Text.Html)
+        }
+        get("/newMessages") {
+            call.respondText(serializeNewMessages(), ContentType.Application.Json)
         }
         static("static") {
             resources()
@@ -44,8 +55,13 @@ private fun renderIndexPage(): String {
     }
 }
 
+private fun serializeNewMessages(): String {
+    val messages = generateRandomMessages(1)
+    return JSON.stringify(messages[0])
+}
+
 fun generateRandomMessages(count: Int): List<Message> {
-    return List(5) {
+    return List(count) {
         Message(generateRandomText(), generateRandomAuthor())
     }
 }
--- frontend/src/main/kotlin/frontend.kt
+++ frontend/src/main/kotlin/frontend.kt
@@ -1,10 +1,64 @@
 package org.jetbrains.kxhtml.isomorphic.frontend
 
+import kotlinx.html.div
+import kotlinx.html.dom.append
+import kotlinx.serialization.json.JSON
+import org.jetbrains.kxhtml.isomorphic.Message
+import org.jetbrains.kxhtml.isomorphic.renderMessage
+import org.w3c.dom.HTMLElement
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.EmptyCoroutineContext
+import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.experimental.suspendCoroutine
+import kotlin.js.Promise
+
+suspend fun httpGet(url: String): String = suspendCoroutine { c ->
+    val xhr = XMLHttpRequest()
+    xhr.onreadystatechange = {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            if (xhr.status / 100 == 2) {
+                c.resume(xhr.response as String)
+            }
+            else {
+                c.resumeWithException(RuntimeException("HTTP error: ${xhr.status}"))
+            }
+        }
+        null
+    }
+    xhr.open("GET", url)
+    xhr.send()
+}
+
+fun <T> async(x: suspend () -> T): Promise<T> {
+    return Promise { resolve, reject ->
+        x.startCoroutine(object : Continuation<T> {
+            override val context = EmptyCoroutineContext
+
+            override fun resume(value: T) {
+                resolve(value)
+            }
+
+            override fun resumeWithException(exception: Throwable) {
+                reject(exception)
+            }
+        })
+    }
+}
+
+fun fetchNewMessages() {
+    async {
+        val data = httpGet("http://localhost:8080/newMessages")
+        val message = JSON.parse<Message>(data)
+        val contentDiv = document.getElementsByClassName("content").item(0) as HTMLElement
+        contentDiv.append.div {
+            renderMessage(message)
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-    window.setInterval({
-        console.log("Update!")
-
-    }, 1000)
+    window.setInterval(::fetchNewMessages, 1000)
 }
