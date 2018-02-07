e589ead2aaa49d1267f078858d6926c726b172b8
Dmitry Jemerov <yole@jetbrains.com>
2018-02-07 11:50:33 +0100

    Show Kotlin github commits instead of random data

--- backend/src/main/kotlin/Server.kt
+++ backend/src/main/kotlin/Server.kt
@@ -3,6 +3,10 @@ package org.jetbrains.kxhtml.isomorphic
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.get
 import io.ktor.content.resources
 import io.ktor.content.static
 import io.ktor.features.CallLogging
@@ -14,17 +18,38 @@ import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import kotlinx.coroutines.experimental.runBlocking
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import kotlinx.serialization.SerialContext
 import kotlinx.serialization.json.JSON
 import java.util.*
 
+data class Committer(val name: String, val date: String)
+
+data class Commit(val message: String, val author: Committer, val committer: Committer)
+
+data class CommitData(val commit: Commit)
+
+val messages = mutableListOf<Message>()
+var lastSentMessageIndex = 0
+
 fun Application.main() {
+    runBlocking {
+        val client = HttpClient(Apache) {
+            install(JsonFeature)
+        }
+
+        val data = client.get<Array<CommitData>>("https://api.github.com/repos/jetbrains/kotlin/commits")
+        data.mapTo(messages) { datum ->
+            Message(datum.commit.message.smartTruncate(), datum.commit.author.name, parseDate(datum.commit.author.date))
+        }
+    }
+
     install(DefaultHeaders)
     install(CallLogging)
     install(StatusPages) {
-        exception<Throwable> { cause ->
+        exception<Throwable> {
             call.respond(HttpStatusCode.InternalServerError)
         }
     }
@@ -47,7 +72,8 @@ fun Application.main() {
 }
 
 private fun renderIndexPage(): String {
-    val messages = generateRandomMessages(5)
+    val messages = messages.take(5)
+    lastSentMessageIndex = 5
     return createHTML().html {
         head {
             script(src = "static/js/frontend.bundle.js") {}
@@ -64,25 +90,12 @@ private fun renderIndexPage(): String {
 }
 
 private fun serializeNewMessages(): String {
-    val messages = generateRandomMessages(1)
-    return messages[0].toJSON()
+    return messages.drop(lastSentMessageIndex++).firstOrNull()?.toJSON() ?: "[]"
 }
 
-fun generateRandomMessages(count: Int): List<Message> {
-    return List(count) {
-        Message(generateRandomText(), generateRandomAuthor(), Date())
-    }
+private fun String.smartTruncate(): String {
+    if (length < 50) return this
+    val index = lastIndexOf(' ', 50)
+    if (index < 0) return substring(0, 50) + "..."
+    return substring(0, index).trim() + "..."
 }
-
-fun generateRandomText() =
-    buildString {
-        for (i in 1..5) {
-            append(((Random().nextInt(26) + 'a'.toInt()).toChar()))
-        }
-    }
-
-val authors = listOf("Alpha", "Bravo", "Charlie", "Delta", "Echo")
-
-fun generateRandomAuthor() =
-    authors[Random().nextInt(5)]
-
